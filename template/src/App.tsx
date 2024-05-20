8ffa515ef1386ac845688c0caa0636b47b8024fd
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-05-20 18:25:46 +0700

    fix: template

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import "./App.css";
 import Footer from "./layouts/components/Footer/Footer";
 import Header from "./layouts/components/Header/Header";
 import RouterCom from "./routers/route";
 
 function App() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'assets/js/main.js';
+        script.type = 'text/javascript';
+        document.body.appendChild(script);
+
+        // Cleanup function to remove the script when the component is unmounted
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <>
             <Header />
--- template/src/pages/Index/components/AuctionWaiting.tsx
+++ template/src/pages/Index/components/AuctionWaiting.tsx
@@ -3,26 +3,24 @@ import SingleAuction from './SingleWaitingAuction'
 const AuctionWaiting = () => {
   return (
     <div className="umino-product_area">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="umino-section_title">
-              <h3>Trang sức sắp được đấu giá</h3>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="umino-section_title">
+            <h3>...</h3>
           </div>
-          <div className="col-lg-12">
-            <div className="umino-product_slider slider-navigation_style-1">
-              <div className="row">
-                <SingleAuction />
-                <SingleAuction />
-                <SingleAuction />
-                <SingleAuction />
-              </div>
-            </div>
+        </div>
+        <div className="col-lg-12">
+          <div className="umino-product_slider slider-navigation_style-1">
+            <SingleAuction/>
+            <SingleAuction/>
+            <SingleAuction/>
+            <SingleAuction/>
           </div>
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
