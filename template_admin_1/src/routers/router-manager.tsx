353a2564f04311e7c6ace5a78c09e84f97fd8b9b
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-05-22 19:05:32 +0700

    upload template admin

--- template_admin_1/src/routers/router-manager.tsx
+++ template_admin_1/src/routers/router-manager.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Index from '../pages/manager/Index/Index'
 import RequestProduct from '../pages/manager/RequestProduct/RequestProduct'
 import AuctionsList from '../pages/manager/Auctions/AuctionsList'
+import CreateAuction from '../pages/manager/Auctions/CreateAuction'
 const RouterComManager = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const RouterComManager = () => {
         <Route path="/manager" element={<Index />} />
         <Route path="/manager/request" element={<RequestProduct />} />
         <Route path="/manager/auction" element={<AuctionsList />} />
+        <Route path="/manager/create-auction" element={<CreateAuction />} />
       </Routes>
     </>
   )
