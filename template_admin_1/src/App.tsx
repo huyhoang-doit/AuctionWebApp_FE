7fbf6e30d29fee066424e1647be54dadd4581eb8
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-05-23 10:00:45 +0700

    fix countdownt

--- template_admin_1/src/App.tsx
+++ template_admin_1/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
 import Footer from './layouts/admin/components/Footer'
 import Header from './layouts/admin/components/Header'
-// import Navbar from './layouts/admin/components/Navbar'
-import Navbar from './layouts/staff/Navbar'
+import Navbar from './layouts/admin/components/Navbar'
+// import Navbar from './layouts/staff/Navbar'
 // import Navbar from './layouts/manager/Navbar'
 import RouterCom from './routers/route'
 
