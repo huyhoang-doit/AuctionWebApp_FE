afd68eecd14099066e6a185bfe4780e8e96429c6
Quang document <lenhatquang27112003@gmail.com>
2024-05-29 11:37:11 +0700

    phantrang

--- template_admin_1/src/App.tsx
+++ template_admin_1/src/App.tsx
@@ -1,17 +1,13 @@
 import './App.css'
 import Footer from './layouts/admin/components/Footer'
 import Header from './layouts/admin/components/Header'
-import Navbar from './layouts/admin/components/Navbar'
-//  import Navbar from './layouts/manager/Navbar'
+// import Navbar from './layouts/admin/components/Navbar'
+ import Navbar from './layouts/manager/Navbar'
 
 import RouterCom from './routers/route'
 
 function App() {
-<<<<<<< HEAD
-  const location = useLocation();
-  const isLoginPage = location.pathname === '/dang-nhap';
-=======
->>>>>>> de93bc1023bd2ab81606c2c8c52f2f1ab620dc93
+
 
   return (
     <>
