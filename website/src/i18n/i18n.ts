8404bf3c5b4f69f104d058cafc5b8adfed7d4b22
Quang document <lenhatquang27112003@gmail.com>
2024-06-18 19:18:15 +0700

    translate sendjewelry

--- website/src/i18n/i18n.ts
+++ website/src/i18n/i18n.ts
@@ -9,6 +9,8 @@ import PageSendJewelry_En from '.././locales/en/PageSendJewelry.json'
 import PageSendJewelry_Vi from '.././locales/vi/PageSendJewelry.json'
 import Contact_En from '.././locales/en/Contact.json'
 import Contact_Vi from '.././locales/vi/Contact.json'
+import MyAccountStaff_En from '.././locales/en/MyAccountStaff.json'
+import MyAccountStaff_Vi from '.././locales/vi/MyAccountStaff.json'
 export const locales = {
   en: 'English',
   vi: 'Tiếng Việt',
@@ -19,13 +21,16 @@ const resources = {
    home: Home_En,  //home is the namespace
    footer: Footer_En,
    PageSendJewelry: PageSendJewelry_En,
-   Contact : Contact_En
+   Contact : Contact_En,
+   MyAccountStaff: MyAccountStaff_En
   },
   vi: {
     home: Home_Vi,
     footer: Footer_Vi,
     PageSendJewelry: PageSendJewelry_Vi,
-    Contact : Contact_Vi
+    Contact : Contact_Vi,
+    MyAccountStaff: MyAccountStaff_Vi
+
   },
 };
 
@@ -34,7 +39,7 @@ const defaultNS = 'home'; //khong truyen namespace thi se lay defaultNS
 i18n.use(initReactI18next).init({
   resources,
   lng: "vi", // ngôn ngữ mặc định
-  ns: ['home', 'footer', 'PageSendJewelry', 'Contact'], // thêm namespace vào đây
+  ns: ['home', 'footer', 'PageSendJewelry', 'Contact','MyAccountStaff'], // thêm namespace vào đây
 //   fallbackLng: "vi", // nếu không nhận được ngôn ngữ nào thì mặc định là tiếng Việt
 defaultNS, //thay fallbackLng
   interpolation: {
